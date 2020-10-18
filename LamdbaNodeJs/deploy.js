afc4ea0352dfe38cb782608cf0329eddce886108
JaGeMo <janmoennig@MacProWLAN.fritz.box>
2020-10-18 20:37:39 +0200

    implemented promises and await/async

--- LamdbaNodeJs/deploy.js
+++ LamdbaNodeJs/deploy.js
@@ -1,8 +1,11 @@
-// Load the SDK and UUID
+// load the SDK and UUID
 var AWS = require('aws-sdk');
 var uuid = require('uuid');
+const { MultiSelect } = require("enquirer");
 
-// Check creds
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+// check creds
 AWS.config.getCredentials(function(err) {
     if (err) console.log(err.stack);
     // crSedentials not loaded
@@ -12,54 +15,106 @@ AWS.config.getCredentials(function(err) {
 });
 
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
-// Create unique bucket name
+// create unique bucket name
 var bucketName = uuid.v4();
-// Create name for uploaded object key
-var keyName = 'checkTest';
+// create name for uploaded object key
 
-s3.createBucket({Bucket: bucketName}, (error, data) => {
-  if (error) {
-      console.log(error);
-    } else {
-      console.log("successfully created bucket");
-      s3.listBuckets((error, data) => {
-        if (error) {
-          console.log("error", error);
-        } else {
-          console.log("successfully retrieved bucket data", data.Buckets);
-          data.Buckets.forEach(element => {
-            s3.deleteBucket({Bucket: element.Name}, (error, data) => {
-              if(error) {
-                console.log("nameE: " + element.Name)
-                console.log("deleting bucket failed");
-              }
-              else {
-                console.log("nameS: " + element.Name)
-                console.log( "deleting bucket " + element.name + " successful");
-                console.log(data);
-              }
-            })  
+function createBucketAndListAllBuckets() {
+  return new Promise((resolve,reject) => {
+    s3.createBucket({Bucket: bucketName}, (err, data) => {
+    if (err) {
+      console.log("\n", err, "\n");
+      reject(err);
+    } 
+    else {
+      console.log("successfully created bucket"); 
+      var loadBuckets = loadBucketList();
+      loadBuckets
+        .then((result) => {
+          result.Buckets.forEach(bucket => {
+            console.log("name: " + bucket.Name);
           });
-          
-        }
+          resolve();
+        })
+        .catch((err) => {
+          console.err(`could not retrieve all: ${err}`);
+        });
+    }})
+  })
+}
+
+const main = async () => {
+  createBucketAndListAllBuckets()
+    .then( async () => {
+      const { Buckets } = await s3.listBuckets().promise();
+      const choices = Buckets.map(({ Name }) => ({ name: Name, value: Name }));
+      const prompt = new MultiSelect({
+        name: "value",
+        message: "Select the buckets you would like to delete",
+        choices
       });
+
+      const bucketsToDelete = await prompt.run();
+      let deletedBuckets = 0;
+      for (let bucket of bucketsToDelete) {
+        await delay(200);
+        const isDeleted = await deleteBucket(bucket);
+        deletedBuckets += isDeleted ? 1 : 0;
+      }
+      console.log(
+        `\nDeleted ${deletedBuckets}/${bucketsToDelete.length} buckets.\n`
+      );
+    })
+}
+
+main();
+
+function loadBucketList() {
+  return new Promise((resolve, reject) => {
+    s3.listBuckets((err, data) => {
+      if(err) {
+        reject(new err("could not retrieve bucket list"))
+      }
+      else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+const deleteBucket = async Bucket => {
+  try {
+    console.log(`deleting ${Bucket}`);
+    // We can't delete a bucket before emptying its contents
+    const { Contents } = await s3.listObjects({ Bucket }).promise();
+    if (Contents.length > 0) {
+      await s3
+        .deleteObjects({
+          Bucket,
+          Delete: {
+            Objects: Contents.map(({ Key }) => ({ Key }))
+          }
+        })
+        .promise();
     }
+    await s3.deleteBucket({ Bucket }).promise();
+    return true;
+  } catch (err) {
+    console.log("\n", err, "\n");
+    return false;
   }
-)
+};
 
-// s3.deleteBucket({Bucket: bucketName}, function(err, data) {
-//   if (err) {
-//       console.log(err);
-//     } else {
-//       console.log("Successfully deleted bucket");
-//     }
-//   }
-// );
 
-// s3.listBuckets(function(error, data) {
-//   if (error) {
-//     console.log("Error", error);
-//   } else {
-//     console.log("Success", data.Buckets);
-//   }
-// });
+function deleteSingleBucket(bucketName) {
+  return new Promise((resolve, reject) => {
+  s3.deleteBucket({Bucket: bucketName}, function(err, data) {
+    if (err) {
+        reject(new err("successfully deleted bucket"));
+      } else {
+        resolve(data);
+      }
+    }
+  );
+  })
+}
